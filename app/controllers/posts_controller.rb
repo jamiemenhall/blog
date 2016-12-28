b18906c429e6cda30cdb4ffa5326ffd8c1ca9a3f
Jamie Menhall <jamiemenhall@mac.com>
2016-12-28 15:47:07 -0500

    saved info from form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,14 @@ class PostsController < ApplicationController
 		@post = Post.new
  	end
  	def create
- 		@post = Post.create(params[:post])
+ 		puts "the params are #{params[:post].inspect}"
+
+ 		# a = params.require(:post).permit(:name, :content)
+ 		# puts "a is #{a.inspect}"
+ 		 @post = Post.create(post_params)
+ 	end
+
+ 	def post_params
+ 		params.require(:post).permit(:name, :content)
  	end
 end
