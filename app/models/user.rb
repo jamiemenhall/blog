3d016f7540ab065a629eabf8bbc24436303972d7
Jamie Menhall <jamiemenhall@mac.com>
2016-12-29 13:09:57 -0500

    added necessary school conditions for allowance

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
 
-  validate :email_is_bu_edu
+  # validate :email_is_bu_edu
 
-  def email_is_bu_edu
-  	if self.email.end_with? "bu.edu"
+  # def email_is_bu_edu
+  # 	if self.email.end_with? "bu.edu"
+  # 	else
+  # 		self.errors.add(:email, "email must end in bu.edu")
+  # 	end
+  # end
+
+  validate :user_went_to_one_of_these_schools
+
+  def user_went_to_one_of_these_schools
+  	if self.email.end_with?("bu.edu") || self.email.end_with?("harvard.edu") || self.email.end_with?("mit.edu")
   	else
-  		self.errors.add(:email, "email must end in bu.edu")
+  		self.errors.add(:email, "you apparantly didn't get into a good school")
   	end
   end
-  
+
+
+
 end
