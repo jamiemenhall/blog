b8d907a935aa33abacdec8568736a5efec585e53
Jamie Menhall <jamiemenhall@mac.com>
2016-12-29 12:49:48 -0500

    restrictive email signup

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
 	validates :name, length: {maximum: 20}
 	belongs_to :user
+
+	validate :name_is_paul
+
+	def name_is_paul
+		if self.name == "paul"
+			#everything is good
+		else
+			self.errors.add(:name, "You should be named Paul")
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
+
+  validate :email_is_bu_edu
+
+  def email_is_bu_edu
+  	if self.email.end_with? "bu.edu"
+  	else
+  		self.errors.add(:email, "email must end in bu.edu")
+  	end
+  end
+  
 end
