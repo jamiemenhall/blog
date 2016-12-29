8064d97ef307792597d6882d1cec22049bf82bce
Jamie Menhall <jamiemenhall@mac.com>
2016-12-29 13:36:40 -0500

    redid conditionary emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,10 +17,23 @@ class User < ApplicationRecord
   validate :user_went_to_one_of_these_schools
 
   def user_went_to_one_of_these_schools
-  	if self.email.end_with?("bu.edu") || self.email.end_with?("harvard.edu") || self.email.end_with?("mit.edu")
-  	else
-  		self.errors.add(:email, "you apparantly didn't get into a good school")
-  	end
+  	endings = ["bu.edu", "harvard.edu", "mit.edu"]
+
+  	# Way 1
+  	email_is_ok = false
+  	endings.each do |a|
+	  	if self.email.end_with? a
+	  		email_is_ok = true
+	  	end
+	end
+
+	self.errors.add(:email, "you apparantly didn't get into a good school") unless email_is_ok
+
+
+	# Way 2
+	# email_suffix = self.email.take_suffix
+	# self.errors.add(:email, "bad email") unless endings.includes? email_suffix
+
   end
 
 
